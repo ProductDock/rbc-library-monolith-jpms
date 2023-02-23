7c6e4b427cb51e4ceb80f2317d1b3b84d8e4c350
Milica Zivkov <milica.zivkov@productdock.com>
2023-02-23 20:03:00 +0100

    Added catalog resource folder as it is in microservice

---
+++ rbc-library-catalog/src/main/resources/migration/V1_3_0__books_fix_script.sql
@@ -0,0 +1,3 @@
+INSERT INTO book (title, author, cover) VALUES ('Idealan timski igrač', 'Patrik Lencioni', 'https://www.delfi.rs/_img/artikli/2019/11/idealan_timski_igrac_vv.jpg');
+
+UPDATE book SET title='Book of Vaadin', author='Vaadin Team', cover='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1353276778l/16157237.jpg'  WHERE id = 37;
