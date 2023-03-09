6b321fb8fc75bea47b423ab7c75f3476a3b5fdc9
nenadbecanovicpd <nenad.becanovic@productdock.com>
2023-03-09 22:01:36 +0100

    Exclude inventory module resources necessary for microservice architecture

--- rbc-library-inventory/src/main/java/com/productdock/library/inventory/config/MicroservicesSecurityConfig.java
+++ rbc-library-inventory/src/main/java/com/productdock/library/inventory/config/MicroservicesSecurityConfig.java
@@ -2,7 +2,6 @@ package com.productdock.library.inventory.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,8 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.interfaces.RSAPublicKey;
 
-@Configuration
-public class SecurityConfig {
+//@Configuration
+public class MicroservicesSecurityConfig {
 
     @Value("${jwt.public.key}")
     RSAPublicKey key;
