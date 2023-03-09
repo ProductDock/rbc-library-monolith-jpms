12533695b4034ea57223472414fa4eb5d76087ac
nenadbecanovicpd <nenad.becanovic@productdock.com>
2023-03-09 22:22:02 +0100

    Add multiple mongodb connection config to rbc-library-inventory module

--- rbc-library-inventory/src/main/java/com/productdock/library/inventory/config/mongodb/InventoryMongoConfig.java
+++ rbc-library-inventory/src/main/java/com/productdock/library/inventory/config/mongodb/InventoryMongoConfig.java
@@ -1,35 +1,31 @@
-package com.productdock.library.user.profiles.config.mongodb;
+package com.productdock.library.inventory.config.mongodb;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 @RequiredArgsConstructor
-public class MongoConfig {
+public class InventoryMongoConfig {
 
-    @Primary
-    @Bean(name = "sessionsDbProperties")
-    @ConfigurationProperties(prefix = "mongodb.sessions")
-    public MongoProperties getSessionsProperties() {
+    @Bean(name = "inventoryDbProperties")
+    @ConfigurationProperties(prefix = "mongodb.inventory")
+    public MongoProperties getInventoryProperties() {
         return new MongoProperties();
     }
 
-    @Primary
-    @Bean(name = SessionsDbMongoConfig.MONGO_TEMPLATE)
-    public MongoTemplate sessionsMongoTemplate() {
-        return new MongoTemplate(primaryFactory(getSessionsProperties()));
+    @Bean(name = InventoryDbMongoConfig.MONGO_TEMPLATE)
+    public MongoTemplate inventoryMongoTemplate() {
+        return new MongoTemplate(secondaryFactory(getInventoryProperties()));
     }
 
     @Bean
-    @Primary
-    public MongoDatabaseFactory primaryFactory(final MongoProperties mongo) {
+    public MongoDatabaseFactory secondaryFactory(final MongoProperties mongo) {
         return new SimpleMongoClientDatabaseFactory(mongo.getUri());
     }
 
